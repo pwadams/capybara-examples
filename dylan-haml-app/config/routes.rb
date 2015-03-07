38db56062d962290581fe8786c5692e59ef087a2
Dylan Ham <dylanham@gmail.com>
2015-03-06 21:52:04 -0700

    spec tests are passable

--- dylan-haml-app/config/routes.rb
+++ dylan-haml-app/config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get 'sign-up', to: 'registrations#new'
-  post 'sign-up', to: 'registrations#create'
-  get 'sign-in', to: 'auth#new'
-  post 'sign-in', to: 'auth#create'
-  get 'sign-out', to: 'auth#destroy'
-  resources :cats
+
 end
--- dylan-haml-app/db/schema.rb
+++ dylan-haml-app/db/schema.rb
@@ -11,25 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307041133) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "cats", force: :cascade do |t|
-    t.string   "name"
-    t.decimal  "age"
-    t.boolean  "friendly"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
 
 end
